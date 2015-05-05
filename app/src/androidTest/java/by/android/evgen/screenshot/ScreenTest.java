018066df0bc5c33c62ea35e2c1d715a198b996b3
unknown <Yauheni_Meshkin@EPBYGROW0104.grodno.epam.com>
2015-05-05 18:33:00 +0300

    add unittest

---
+++ app/src/androidTest/java/by/android/evgen/screenshot/ScreenTest.java
@@ -0,0 +1,96 @@
+package by.android.evgen.screenshot;
+
+import android.app.Instrumentation;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.view.View;
+
+import com.robotium.solo.Solo;
+
+import junit.framework.Assert;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Yauheni_Meshkin on 5/5/2015.
+ */
+public class ScreenTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+    private MainActivity mainActivity;
+
+    public ScreenTest() {
+        super(MainActivity.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+        mainActivity = getActivity();
+    }
+
+    public void testActivity() {
+        solo.waitForActivity(by.android.evgen.screenshot.MainActivity.class);
+        View view = mainActivity.getWindow().getDecorView().getRootView();
+        view.setDrawingCacheEnabled(true);
+        try {
+            takeScreenShot(view, "testRecorded_1316975601089");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+        assertEquals(true, true);
+    }
+
+    public void testCase() {
+        String testCaseName = String.format("%s.%s", getClass().getName(), getName());
+        Log.d("***", "****" + getClass().getName() + "***" + getName());
+        solo.takeScreenshot(testCaseName);
+//        solo.goBackToActivity("by.android.evgen.screenshot.ScreenActivity");
+//        solo.getCurrentActivity();
+//        solo.startScreenshotSequence (String) / startScreenshotSequence (String, Int, Int, Int);
+//        solo.stopScreenshotSequence ();
+//        solo.getScreenshotSequence (String, Boolean, String, String);
+        Assert.assertTrue(true);
+    }
+
+    public void takeScreenShot(final View view, final String name)
+            throws Exception {
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("GO save");
+                view.setDrawingCacheEnabled(true);
+                view.buildDrawingCache();
+                Bitmap b = view.getDrawingCache();
+                FileOutputStream fos = null;
+                try {
+                    final String path = Environment.getExternalStorageDirectory() + "/Pictures/Screenshots/";
+                    File dir = new File(path);
+                    if (!dir.mkdirs()) {
+                        System.out.println("Creaet sd card failed");
+                    }
+
+                    if (!dir.exists()) {
+                        System.out.println(path);
+                        dir.mkdirs();
+                    }
+
+                    fos = new FileOutputStream(path + name + ".jpg");
+                    if (fos != null) {
+                        b.compress(Bitmap.CompressFormat.JPEG, 90, fos);
+                        fos.close();
+                    }
+                } catch (IOException e) {
+                }
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/by/android/evgen/screenshot/ScreenActivity.java
+++ app/src/main/java/by/android/evgen/screenshot/ScreenActivity.java
@@ -7,6 +7,15 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.opengles.GL;
+import javax.microedition.khronos.opengles.GL10;
+
 /**
  * Created by evgen on 04.05.2015.
  */
@@ -26,10 +35,46 @@ public class ScreenActivity extends Activity {
 
 //                solo.waitForActivity("com.botskool.DialogBox.DialogBox", ACTIVITY_WAIT_MILLIS);
 
-                Bitmap bitmap;
-                View v1 = ScreenActivity.this.getWindow().getDecorView();
-                v1.setDrawingCacheEnabled(true);
-                bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+                int width =  container.getWidth() ;
+                int height =  container.getHeight() ;
+                int screenshotSize = width * height ;
+                ByteBuffer bb =  ByteBuffer. allocateDirect(screenshotSize * 4);
+                bb . order ( ByteOrder. nativeOrder());
+                EGL10 egl = (EGL10) EGLContext.getEGL();
+                GL10 gl = (GL10) egl.eglGetCurrentContext().getGL();
+                gl.glReadPixels(0, 0, width, height, GL10.GL_RGBA,
+                        GL10.GL_UNSIGNED_BYTE, bb);
+                int pixelsBuffer []  =  new  int [ screenshotSize ];
+                bb . asIntBuffer (). get ( pixelsBuffer );
+                bb =  null ;
+                Bitmap bitmap =  Bitmap . createBitmap ( width , height ,
+                        Bitmap . Config . RGB_565 );
+                bitmap . setPixels ( pixelsBuffer , screenshotSize - width ,  - width ,  0 ,
+                        0 , width , height );
+                pixelsBuffer =  null ;
+
+                short sBuffer []  =  new  short [ screenshotSize ];
+                ShortBuffer sb =  ShortBuffer . wrap ( sBuffer );
+                bitmap . copyPixelsToBuffer ( sb );
+
+
+                for  ( int i =  0 ; i < screenshotSize ;  ++ i )  {
+                    short v = sBuffer [ i ];
+                    sBuffer [ i ]  =  ( short )  ((( v &  0x1f )  <<  11 )  |  ( v &  0x7e0 )  |  (( v &  0xf800 )  >>  11 ));
+                }
+                sb . rewind ();
+                bitmap . copyPixelsFromBuffer ( sb );
+//                container . captureBmp = bitmap . copy ( Bitmap . Config . ARGB_8888 , false );
+//                MainImageProcessingActivity . capture = false ;
+//
+//
+//
+//
+//
+//                Bitmap bitmap;
+//                View v1 = ScreenActivity.this.getWindow().getDecorView();
+//                v1.setDrawingCacheEnabled(true);
+//                bitmap = Bitmap.createBitmap(v1.getDrawingCache());
 //                screenIcon.setImageBitmap(bitmap);
                 screenIcon.setImageBitmap(bitmap);
             }
